3868b1cdd07877d4f70f089021c931bb6148111c
Daniil <50438498+ctbip@users.noreply.github.com>
2023-04-13 19:41:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import numpy as np
+import math
+from scipy.stats import norm
+from scipy.stats import binomtest
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 784892881 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+def solution(x_success: int,            # Количество продаж на контроле
+             x_cnt: int,                # Количество заявок на контроле
+             y_success: int,            # Количество продаж на тесте
+             y_cnt: int) -> bool:       # Количество заявок на тесте
+
+    control_conversion = x_success / x_cnt
+    test_conversion = y_success / y_cnt
+    
+    # perform two-sided hypothesis test with alpha significance level
+    p_value = binomtest(y_success, n=y_cnt, p=control_conversion).pvalue
+    
+    alpha = 0.07
+    reject_null = p_value < alpha
+    
+    return reject_null
